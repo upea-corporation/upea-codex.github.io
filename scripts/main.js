0dd2ee2639fef961f93f3fd1ec1f8fbcc2e506fa
Gabriel A.K.C <irdurdev@hotmail.com>
2025-08-15 16:41:51 +0200

    Actualización - 1641pm15082025

--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Mensaje inicial de la terminal (incluyendo el copyright)
     const initialMessageContent = `UPEA Codex - Sistema de Cifrado Cuántico
-Versión 1.0.3 (Build 250524)
+Versión 2.70520.25 (Build 173343)
 (c) 2025 UPEA Corp. Todos los derechos reservados.
 
 Escribe 'help' para ver los comandos disponibles.
